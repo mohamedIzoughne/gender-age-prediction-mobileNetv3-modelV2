c1e452eda742e4d2df95b6de9615b89fb99effd4
izourne <izourne003@gmail.com>
2026-07-31 10:19:42 +0100

    update the classes

--- src/models/mobilenet/classifier.py
+++ src/models/mobilenet/classifier.py
@@ -199,10 +199,10 @@ class AgeGenderClassifier(pl.LightningModule):
         
         AGE_CLASSES = [
             {"id": 0, "label": "dependent_minor", "min_age": 0, "max_age": 17},
-            {"id": 1, "label": "young_aspirational", "min_age": 18, "max_age": 25},
-            {"id": 2, "label": "independent_professional", "min_age": 26, "max_age": 35},
-            {"id": 3, "label": "early_nesting", "min_age": 36, "max_age": 45},
-            {"id": 4, "label": "peak_earning_adult", "min_age": 46, "max_age": 60},
+            {"id": 1, "label": "young_aspirational", "min_age": 18, "max_age": 28},
+            {"id": 2, "label": "independent_professional", "min_age": 29, "max_age": 38},
+            {"id": 3, "label": "early_nesting", "min_age": 39, "max_age": 48},
+            {"id": 4, "label": "peak_earning_adult", "min_age": 49, "max_age": 60},
             {"id": 5, "label": "mature_provider", "min_age": 60, "max_age": 100},
             # {"id": 6, "label": "pre_retirement", "min_age": 56, "max_age": 65},
             # {"id": 7, "label": "senior_consumer", "min_age": 66, "max_age": 120},
